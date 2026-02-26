8af3e4229fcf6ee851d80310d4015a3a17c400bc
Shri <110012624+pshri5@users.noreply.github.com>
2026-02-26 15:58:43 +0530

    Created db connection

--- src/app.js
+++ src/app.js
@@ -9,3 +9,6 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv"
+import { connectDB } from "./db"
+import app from "./app"
 
 
+const port = process.env.PORT
 
 dotenv.config({
     path: "./.env"
+})
+
+connectDB()
+.then(()=>{
+    app.listen(prompt,()=>{
+        console.log(`Server is running on port ${port} `)
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection error",err)
 })
