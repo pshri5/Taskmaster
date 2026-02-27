a64dd7982f83be665b9688babd0fadbe5da7658c
Shri <110012624+pshri5@users.noreply.github.com>
2026-02-27 10:35:07 +0530

    Update User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,7 +42,39 @@ const userSchema = new Schema({
 
 },{timestamps:true});
 
+//Hashing password before saving
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+//Comparing passwords
+userSchema.methods.isPasswordCorrect = async function(inputPassword){
+    return await bcrypt.compare(inputPassword,this.password)
+} 
 
+userSchema.methods.generateAccessToken = function(){
+ return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name
+ },
+ process.env.ACCESS_TOKEN_SECRET,
+ {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+)   
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
 
 
 
